5b2c65cd41de489ab943fd5d75e2d13bcbb317ee
leoiltizio <Leosca9@gmail.com>
2023-12-05 08:10:44 +0000

    added the animation for  the main character
    locked camera, added annimation flipbook to character, still need to optimize it, would be cool if the sprite flipped when moving in a different direction

--- VerticalGJ_Group1/Intermediate/Build/Win64/UnrealEditor/Inc/VerticalGJ_Group1/UHT/Enemy.generated.h
+++ VerticalGJ_Group1/Intermediate/Build/Win64/UnrealEditor/Inc/VerticalGJ_Group1/UHT/Enemy.generated.h
@@ -14,18 +14,18 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define VERTICALGJ_GROUP1_Enemy_generated_h
 
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_24_GENERATED_BODY \
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_24_GENERATED_BODY \
 	friend struct Z_Construct_UScriptStruct_FEnemyProperties_Statics; \
 	VERTICALGJ_GROUP1_API static class UScriptStruct* StaticStruct();
 
 
 template<> VERTICALGJ_GROUP1_API UScriptStruct* StaticStruct<struct FEnemyProperties>();
 
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_SPARSE_DATA
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_RPC_WRAPPERS
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_RPC_WRAPPERS_NO_PURE_DECLS
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ACCESSORS
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_INCLASS_NO_PURE_DECLS \
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_SPARSE_DATA
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_RPC_WRAPPERS
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ACCESSORS
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAEnemy(); \
 	friend struct Z_Construct_UClass_AEnemy_Statics; \
@@ -34,7 +34,7 @@ public: \
 	DECLARE_SERIALIZER(AEnemy)
 
 
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_INCLASS \
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_INCLASS \
 private: \
 	static void StaticRegisterNativesAEnemy(); \
 	friend struct Z_Construct_UClass_AEnemy_Statics; \
@@ -43,7 +43,7 @@ public: \
 	DECLARE_SERIALIZER(AEnemy)
 
 
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_STANDARD_CONSTRUCTORS \
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API AEnemy(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEnemy) \
@@ -57,7 +57,7 @@ public: \
 	NO_API virtual ~AEnemy();
 
 
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ENHANCED_CONSTRUCTORS \
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API AEnemy(AEnemy&&); \
@@ -69,27 +69,27 @@ public: \
 	NO_API virtual ~AEnemy();
 
 
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_53_PROLOG
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_GENERATED_BODY_LEGACY \
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_53_PROLOG
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_SPARSE_DATA \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_RPC_WRAPPERS \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ACCESSORS \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_INCLASS \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_STANDARD_CONSTRUCTORS \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_SPARSE_DATA \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_RPC_WRAPPERS \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ACCESSORS \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_INCLASS \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_GENERATED_BODY \
+#define FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_SPARSE_DATA \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_RPC_WRAPPERS_NO_PURE_DECLS \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ACCESSORS \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_INCLASS_NO_PURE_DECLS \
-	FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ENHANCED_CONSTRUCTORS \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_SPARSE_DATA \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ACCESSORS \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_INCLASS_NO_PURE_DECLS \
+	FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h_56_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -97,7 +97,7 @@ PRAGMA_ENABLE_DEPRECATION_WARNINGS
 template<> VERTICALGJ_GROUP1_API UClass* StaticClass<class AEnemy>();
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FID_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h
+#define CURRENT_FILE_ID FID_Group1_VerticalGJ_VerticalGJ_Group1_Source_VerticalGJ_Group1_Enemy_h
 
 
 #define FOREACH_ENUM_EENEMYTYPE(op) \
